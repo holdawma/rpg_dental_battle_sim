a1900e459b2a2148cba354a2845932e8b65b30e9
holdawma <52386715+holdawma@users.noreply.github.com>
2019-10-20 16:02:51 +1300

    Dental Hygiene Battle sim v0.80
    - Added 3 more normal enemies with unique names and stats
    - Added a final boss with boosted stats to the end of the game
    - Balance changes to special abilities and healing

--- Dental Hygiene Battle sim.py
+++ Dental Hygiene Battle sim.py
@@ -7,7 +7,8 @@
 # v0.65: 10/09/19
 # v0.66: 23/09/19
 # v0.67: 15/10/19
-# v0.68: 18/10/19
+# v0.70: 19/10/19
+# v0.80: 20/10/19
 
 import random
 import time
@@ -141,8 +142,8 @@ CHOOSE A HEAL:
     3) Visit Dentist
        - Costs 15 Mana
        - Regenerates 60 Health
-    NOTE: Healing is significantly less effective when above 100hp
-       """)
+    NOTE: Healing is significantly less effective when above max hp
+""")
                 # Cancel Option
                 elif heal_move == "5":
                     heal_again = False
@@ -169,8 +170,8 @@ CHOOSE AN ABILITY:
 
                 # Floss Move
                 if special_move == "1":
-                    if player_mp >= 15:
-                        player_mp -= 15
+                    if player_mp >= 10:
+                        player_mp -= 10
                         enemy_hp -= 30
                         print("You used Floss and dealt 30 damage!")
                         player_hp, player_mp, enemy_hp, enemy_mp = enemy_turn(player_hp, player_rst, player_dmg, player_mp, enemy_name, enemy_hp, enemy_rst, enemy_dmg, enemy_mp)
@@ -180,20 +181,20 @@ CHOOSE AN ABILITY:
 
                 # Restock Toothpaste Move
                 elif special_move == "2":
-                    player_mp += 10
-                    if player_mp >= player_max_hp:
-                        player_mp = player_max_hp
+                    player_mp += 15
+                    if player_mp >= player_max_mp:
+                        player_mp = player_max_mp
                     print("You used Restock Toothpaste and regenerated 10 mana!\nYou now have {} mana".format(player_mp))
                     player_hp, player_mp, enemy_hp, enemy_mp = enemy_turn(player_hp, player_rst, player_dmg, player_mp, enemy_name, enemy_hp, enemy_rst, enemy_dmg, enemy_mp)
                     spec_again = False
 
                 # Reduce Sugar Move
                 elif special_move == "3":
-                    if player_mp >= 20:
-                        player_mp -= 20
-                        player_hp += 15
-                        enemy_hp -= 15
-                        print("You used Reduce Sugar, regenerated 15 health and dealt 15 damage!")
+                    if player_mp >= 15:
+                        player_mp -= 15
+                        player_hp += 20
+                        enemy_hp -= 20
+                        print("You used Reduce Sugar, regenerated 20 health and dealt 20 damage!")
                         player_hp, player_mp, enemy_hp, enemy_mp = enemy_turn(player_hp, player_rst, player_dmg, player_mp, enemy_name, enemy_hp, enemy_rst, enemy_dmg, enemy_mp)
                         spec_again = False
                     else:
@@ -204,16 +205,16 @@ CHOOSE AN ABILITY:
                     print("""------------------------------
 
     1) Floss
-        - Costs 15 Mana
+        - Costs 10 Mana
         - Deals 30 Damage
 
     2) Restock Toothpaste
-        - Regenerates 10 Mana
+        - Regenerates 15 Mana
 
     3) Reduce Sugar
-        - Costs 20 Mana
-        - Deals 15 Damage
-        - Regenerates 15 Health
+        - Costs 15 Mana
+        - Deals 20 Damage
+        - Regenerates 20 Health
 """)
                 elif special_move == "5":
                     spec_again = False
@@ -231,7 +232,7 @@ Resistance -> The percentage that damage received is reduced by, special abiliti
 
 Damage -> How much damage regular attacks deal, this can vary by up to 5 damage higher or lower than the shown value.
 
-Mana -> Special abilities use mana, if the unit does not have enough mana to use a special ability then it cannot be used.
+Mana -> Special abilities and heals use mana, if the unit does not have enough mana to use an ability then it cannot be used.
 ------------------------------
 """)
         else:
@@ -246,7 +247,7 @@ Mana -> Special abilities use mana, if the unit does not have enough mana to use
             print("Try again!")
             for i in range(12):
                 print("\n")
-            time.sleep(1.3)
+            time.sleep(2)
             for i in range(25):
                 print("\n")
             main()
@@ -260,6 +261,14 @@ Mana -> Special abilities use mana, if the unit does not have enough mana to use
 
 def items(player_max_hp, player_rst, player_dmg, player_max_mp):
     """A function to let the user choose items to upgrade their character"""
+
+    # Showing the player their current stats
+    print("""PLAYER STATS:
+        Max Health: {}
+        Resistance: {}
+        Damage: {}
+        Max Mana: {}
+    """.format(player_max_hp, player_rst, player_dmg, player_max_mp))
     
     # Dictionary with all items in it
     all_items = {1 : ("Toothbrush of the Strong", 20, 5, -5, 0), 2 : ("Sharp Fillings", 0, 0, 10, -10), 3 : ("Hardened Braces", 15, 0, 0, -10), 4 : ("Arcane Floss", 0, -5, 0, 15), 5 : ("Mouthwash of Resistance", -20, 15, 0, 0), 6 : ("Ethereal Floss", 0, 15, -5, -10), 7 : ("Soul Fillings", -15, 0, 10, 0), 8 : ("Braces of Power", -10, 0, 0, 20), 9 : ("Toothbrush of the Magi", 0, -5, -5, 25), 10 : ("Rite of the Orthodontist", 20, 10, 15, player_max_mp*-1)}
@@ -300,7 +309,7 @@ Note: Stat values displayed are total values after item selection
         Resistance :{i3rst}
         Damage     :{i3dmg}
         Max Mana   :{i3mp}
-------------------------------""".format(item1 = all_items[selected_items[0]][0], item2 = all_items[selected_items[1]][0], item3 = all_items[selected_items[2]][0], i1hp = player_max_hp + all_items[selected_items[0]][1], i1rst = player_rst + all_items[selected_items[0]][2], i1dmg = player_dmg + all_items[selected_items[0]][3], i1mp = player_max_mp + all_items[selected_items[0]][4], i2hp = player_max_hp + all_items[selected_items[1]][1], i2rst = player_rst + all_items[selected_items[1]][2], i2dmg = player_dmg + all_items[selected_items[1]][3], i2mp = player_max_mp + all_items[selected_items[1]][4], i3hp = player_max_hp + all_items[selected_items[2]][1], i3rst = player_rst + all_items[selected_items[2]][2], i3dmg = player_dmg + all_items[selected_items[2]][3], i3mp = player_max_mp + all_items[selected_items[2]][4]) 
+------------------------------""".format(item1 = all_items[selected_items[0]][0], item2 = all_items[selected_items[1]][0], item3 = all_items[selected_items[2]][0], i1hp = player_max_hp + all_items[selected_items[0]][1], i1rst = player_rst + all_items[selected_items[0]][2], i1dmg = player_dmg + all_items[selected_items[0]][3], i1mp = player_max_mp + all_items[selected_items[0]][4], i2hp = player_max_hp + all_items[selected_items[1]][1], i2rst = player_rst + all_items[selected_items[1]][2], i2dmg = player_dmg + all_items[selected_items[1]][3], i2mp = player_max_mp + all_items[selected_items[1]][4], i3hp = player_max_hp + all_items[selected_items[2]][1], i3rst = player_rst + all_items[selected_items[2]][2], i3dmg = player_dmg + all_items[selected_items[2]][3], i3mp = player_max_mp + all_items[selected_items[2]][4])
     
     print(item_printing)
 
@@ -310,7 +319,7 @@ Note: Stat values displayed are total values after item selection
         if item_choice == "1":
             player_max_hp += all_items[selected_items[0]][1]
             player_rst += all_items[selected_items[0]][2]
-            player_dmg += all_items[selected_items[0]][3]
+            player_dmg += all_items[selected_items[0]][3] 
             player_max_mp += all_items[selected_items[0]][4]
             again = False
         elif item_choice == "2":
@@ -342,7 +351,7 @@ def main():
     """Main"""
     player_max_hp = 100
     player_hp = 100
-    player_rst = 10
+    player_rst = 20
     player_dmg = 20
     player_max_mp = 50
     player_mp = 50
@@ -360,40 +369,71 @@ def main():
 |____/ \__,_|\__|\__|_|\___| |_____/|_|_| |_| |_|\__,_|_|\__,_|\__\___/|_|   
                                                                                   
                                                                                   """)
+
+    # First Battle
     print("Welcome to the Dental Hygiene Battle Simulator")
+    time.sleep(1.5)
+    print("You are defending Dental Hygiene from evil foods!")
     time.sleep(1.5)
     print("Look out!\nYou are being attacked by an Evil Milkshake!")
     time.sleep(1.5)
-    player_max_hp, player_hp, player_rst, player_dmg, player_max_mp, player_mp = battle(player_max_hp, player_hp, player_rst, player_dmg, player_max_mp, player_mp, "Evil Milkshake", 50, 10, 15, 25)
-    print("""PLAYER STATS:
-        Max Health: {}
-        Resistance: {}
-        Damage: {}
-        Max Mana: {}
-    """.format(player_max_hp, player_rst, player_dmg, player_max_mp))
+    player_max_hp, player_hp, player_rst, player_dmg, player_max_mp, player_mp = battle(player_max_hp, player_hp, player_rst, player_dmg, player_max_mp, player_mp, "Evil Milkshake", 50, 15, 15, 25)
     
     player_max_hp, player_rst, player_dmg, player_max_mp = items(player_max_hp, player_rst, player_dmg, player_max_mp)
     player_hp = player_max_hp
     player_mp = player_max_mp
 
+    # Second Battle
     print("I hope you're ready for another battle")
     time.sleep(1)
     print("Because there is no time to rest when fighting for dental hygiene!")
     time.sleep(1.5)
     print("You are being attacked by a Corrupt Soda!")
-    player_max_hp, player_hp, player_rst, player_dmg, player_max_mp, player_mp = battle(player_max_hp, player_hp, player_rst, player_dmg, player_max_mp, player_mp, "Corrupt Soda", 50, 35, 20, 40)
+    time.sleep(1)
+    player_max_hp, player_hp, player_rst, player_dmg, player_max_mp, player_mp = battle(player_max_hp, player_hp, player_rst, player_dmg, player_max_mp, player_mp, "Corrupt Soda", 50, 40, 20, 40)
 
-    print("""PLAYER STATS:
-        Max Health: {}
-        Resistance: {}
-        Damage: {}
-        Max Mana: {}
-    """.format(player_max_hp, player_rst, player_dmg, player_max_mp))
+    player_max_hp, player_rst, player_dmg, player_max_mp = items(player_max_hp, player_rst, player_dmg, player_max_mp)
+    player_hp = player_max_hp
+    player_mp = player_max_mp
+
+    # Third Battle
+    print("You better have found that easy!")
+    time.sleep(1)
+    print("The enemies are only going to get tougher!")
+    time.sleep(1.5)
+    print("Here comes a Rotten Snack Bar!")
+    time.sleep(1)
+    player_max_hp, player_hp, player_rst, player_dmg, player_max_mp, player_mp = battle(player_max_hp, player_hp, player_rst, player_dmg, player_max_mp, player_mp, "Rotten Snack Bar", 100, 20, 30, 50)
     
     player_max_hp, player_rst, player_dmg, player_max_mp = items(player_max_hp, player_rst, player_dmg, player_max_mp)
     player_hp = player_max_hp
     player_mp = player_max_mp
 
+    # Fourth Battle
+    print("You truly are the best Dental Health defender we have ever seen")
+    time.sleep(1.5)
+    print("But the job is not done yet! There are still many threats to Dental Hygiene for you to defeat!")
+    time.sleep(2)
+    print("You are under attack by a Wicked Bag of Crisps!")
+    time.sleep(1.5)
+    player_max_hp, player_hp, player_rst, player_dmg, player_max_mp, player_mp = battle(player_max_hp, player_hp, player_rst, player_dmg, player_max_mp, player_mp, "Wicked Bag of Crisps", 200, 0, 25, 20)
 
+    player_max_hp, player_rst, player_dmg, player_max_mp = items(player_max_hp, player_rst, player_dmg, player_max_mp)
+    player_hp = player_max_hp
+    player_mp = player_max_mp
+
+    # Final Boss
+    print("You may have found that easy")
+    time.sleep(1)
+    print("But the biggest challenge is yet to come")
+    time.sleep(1)
+    print("You must now go and face the final boss")
+    time.sleep(1)
+    print("You are being attacked by The Deadly Sweets!")
+    time.sleep(1)
+    player_max_hp, player_hp, player_rst, player_dmg, player_max_mp, player_mp = battle(player_max_hp, player_hp, player_rst, player_dmg, player_max_mp, player_mp, "The Deadly Sweets", 200, 35, 30, 40)
+
+    print("You successfully defended Dental Hygiene from all the evil foods!")
+    print("Congratulations!")
 
 main()
